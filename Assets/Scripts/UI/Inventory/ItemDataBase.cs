8100122b83a757cd316bc119e3cd58ae23dcd3ca
Wilson Wong <pollyhkyau@hotmail.com>
2016-01-14 22:23:20 -0800

    Inventory UI Work
    Created UI Info Panel Interface

---
+++ Assets/Scripts/UI/Inventory/ItemDataBase.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ItemDataBase : MonoBehaviour
+{
+    public List<Item> itemDatabase;
+}
+
+[System.Serializable]
+public class Item
+{
+    public string itemName;
+    public string description;
+    public string type;
+    public bool stackable;
+    public Image itemImage;
+    public GameObject itemObject;
+    public float weight;
+    public int value;
+
+}
+
+
